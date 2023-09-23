3c3f112b2746fd89587c7877ed426f40867d6acc
Abeke <abekeasian@gmail.com>
2023-09-23 11:40:01 +0200

    installed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
         <div className="page-container">
             <img src={logo} alt="Company logo"/>
             <h1>Begin hier met het maken van jouw blog-applicatie!</h1>
+            <p>dd</p>
         </div>
     )
 }
